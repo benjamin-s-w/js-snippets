d2108506db8eb1a6e9fb7e7c6a99d58e6aeeaa72
Benjamin Woodruff <benjamin-s-w@users.noreply.github.com>
2022-06-08 23:28:05 -0400

    Create get-screen-dimensions.js

---
+++ get-screen-dimensions.js
@@ -0,0 +1,6 @@
+// get dimensions of user's screen and log it
+
+const width = screen.width;
+const height = screen.height;
+const screenDimensions = `${width}x${height}`;
+console.log(screenDimensions);
